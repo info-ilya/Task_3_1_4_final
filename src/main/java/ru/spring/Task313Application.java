1bd9db9ac18c0d75a70efa61cd4a6ae4a9354631
ilya <fire-88@mail.ru>
2020-11-06 18:05:45 +0300

    v.1.0.1

--- src/main/java/ru/spring/Task313Application.java
+++ src/main/java/ru/spring/Task313Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Task312Application {
+public class Task313Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Task312Application.class, args);
+        SpringApplication.run(Task313Application.class, args);
     }
 
 }
