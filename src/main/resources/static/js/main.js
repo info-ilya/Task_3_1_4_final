a6c086e8b39b1c1a71f5dc87d1e132676246f817
info-ilya <fire-88@mail.ru>
2020-11-07 12:12:28 +0300

    v.1.0.3

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,66 +1,70 @@
-$('#myList a').on('click', function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-});
-$(function () {
-    $('#myList a:first-child').tab('show');
-});
-
 $(document).ready(function () {
-    $('.table .eBtn').on('click', function (event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        var text = $(this).text();
-        if (text === 'Edit') {
-            $.get(href, function (user, status) {
-                $('.myModal #id').val(user.id);
-                $('.myModal #firstName').val(user.firstName);
-                $('.myModal #lastName').val(user.lastName);
-                $('.myModal #age').val(user.age);
-                $('.myModal #email').val(user.email);
-                $('.myModal #password').val(user.password);
+    $('#myList a').on('click', function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+    $(function () {
+        $('#myList a:first-child').tab('show');
+    });
 
-                let roleAdmin = null;
-                let roleUser = null;
+    function editModal() {
+        $('.table .eBtn').on('click', function (event) {
+            event.preventDefault();
+            let href = $(this).attr('href');
+            let text = $(this).text();
+            if (text === 'Edit') {
+                $.get(href, function (user, status) {
+                    $('.myModal #id').val(user.id);
+                    $('.myModal #firstName').val(user.firstName);
+                    $('.myModal #lastName').val(user.lastName);
+                    $('.myModal #age').val(user.age);
+                    $('.myModal #email').val(user.email);
+                    $('.myModal #password').val(user.password);
 
-                $('.myModal #roles').empty();
+                    let roleAdmin = null;
+                    let roleUser = null;
 
-                $.each(user.roles, function (i, role) {
-                    // $('.myModal #roles').append('<option value="' + i + '">' + role.name + '</option>');
-                    //$('.myModal #roles').append('<option selected value="' + role.name + '">' + role.name + '</option>');
-                    if (role.name === 'ADMIN') {
-                        roleAdmin = "ADMIN";
-                    } else {
-                        roleUser = "USER";
+                    $('.myModal #roles').empty();
+
+                    $.each(user.roles, function (i, role) {
+                        // $('.myModal #roles').append('<option value="' + i + '">' + role.name + '</option>');
+                        //$('.myModal #roles').append('<option selected value="' + role.name + '">' + role.name + '</option>');
+                        if (role.name === 'ADMIN') {
+                            roleAdmin = "ADMIN";
+                        } else {
+                            roleUser = "USER";
+                        }
+                    });
+                    if (roleUser === 'USER') {
+                        $('.myModal #roles').append('<option selected value="USER">USER</option>').append('<option value="ADMIN">ADMIN</option>');
+                    } else if (roleAdmin === 'ADMIN') {
+                        $('.myModal #roles').append('<option selected value="ADMIN">ADMIN</option>').append('<option value="USER">USER</option>');
                     }
+
+                })
+                $('#exampleModal').modal();
+            }
+        });
+    }
+
+    function deleteModal() {
+        $('.table .delBtn').on('click', function (event) {
+            event.preventDefault();
+            let href = $(this).attr('href');
+            $.get(href, function (user, status) {
+                $('.deleteModalNew #id1').val(user.id);
+                $('.deleteModalNew #firstName1').val(user.firstName);
+                $('.deleteModalNew #lastName1').val(user.lastName);
+                $('.deleteModalNew #age1').val(user.age);
+                $('.deleteModalNew #email1').val(user.email);
+                $('.deleteModalNew #roles1').empty();
+                $.each(user.roles, function (i, role) {
+                    $('.deleteModalNew #roles1').append('<option value="' + role.name + '">' + role.name + '</option>');
                 });
-                if (roleUser === 'USER') {
-                    $('.myModal #roles').append('<option selected value="USER">USER</option>').append('<option value="ADMIN">ADMIN</option>');
-                } else if (roleAdmin === 'ADMIN') {
-                    $('.myModal #roles').append('<option selected value="ADMIN">ADMIN</option>').append('<option value="USER">USER</option>');
-                }
-
             })
-            $('#exampleModal').modal();
-        }
-    });
-
-    $(' .table .delBtn').on('click', function (event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        $.get(href, function (user, status) {
-            $('.deleteModalNew #id1').val(user.id);
-            $('.deleteModalNew #firstName1').val(user.firstName);
-            $('.deleteModalNew #lastName1').val(user.lastName);
-            $('.deleteModalNew #age1').val(user.age);
-            $('.deleteModalNew #email1').val(user.email);
-            $('.deleteModalNew #roles1').empty();
-            $.each(user.roles, function (i, role) {
-                $('.deleteModalNew #roles1').append('<option value="' + role.name + '">' + role.name + '</option>');
-            });
-        })
-        $('#deleteModal').modal();
-    });
+            $('#deleteModal').modal();
+        });
+    }
 
     // function createUser() {
     //     fetch('http://localhost:8080/create', {
@@ -88,32 +92,15 @@ $(document).ready(function () {
     //         });
     // }
 
-
-    // DO GET
-    // function ajaxGet(){
-    //     $.ajax({
-    //         type : "GET",
-    //         url : "http://localhost:8080/all",
-    //         success: function(user){
-    //             var taskRow = '<tr>' +
-    //                 '<td>' + user.id + '</td>' +
-    //                 '<td>' + user.firstName + '</td>' +
-    //                 '<td>' + user.lastName + '</td>' +
-    //                 '<td>' + user.age + '</td>' +
-    //                 '<td>' + user.email + '</td>' +
-    //                 '<td>' + user.roles + '</td>' +
-    //                 '</tr>';
-    //             $('#userstable tbody').append(taskRow);
-    //
-    //         },
-    //     });
-    // }
-
     $.getJSON("http://localhost:8080/all",
         function (data) {
-            var userTable = '';
 
+            let userTable = '';
             let arr = [];
+            let editBtn =
+                '<a href="/findone/?id=userID" class="btn btn-info btn-sm eBtn">Edit</a>';
+            let deleteBtn =
+                '<a href="/findone/?id=userID" class="btn btn-danger btn-sm delBtn">Delete</a>';
 
             $.each(data, function (key, user) {
 
@@ -122,28 +109,20 @@ $(document).ready(function () {
                 });
 
                 userTable += '<tr>';
-
-                userTable += '<td>' +
-                    user.id + '</td>';
-
-                userTable += '<td>' +
-                    user.firstName + '</td>';
-
-                userTable += '<td>' +
-                    user.lastName + '</td>';
-
-                userTable += '<td>' +
-                    user.email + '</td>';
-
-                userTable += '<td>' +
-                    user.age + '</td>';
-
-                userTable += '<td>' + arr + '</td>';
-
+                userTable += '<td id="userID">' + user.id + '</td>';
+                userTable += '<td id="userFirstName">' + user.firstName + '</td>';
+                userTable += '<td id="userLastName">' + user.lastName + '</td>';
+                userTable += '<td id="userEmail">' + user.email + '</td>';
+                userTable += '<td id="userAge">' + user.age + '</td>';
+                userTable += '<td id="userRoles">' + arr + '</td>';
+                userTable += '<td id="userEditBtn">' + editBtn.replace('userID', user.id) + '</td>';
+                userTable += '<td id="userDeleteBtn">' + deleteBtn.replace('userID', user.id) + '</td>';
                 userTable += '</tr>';
             });
-
             $('#userstable').append(userTable);
-        });
 
+            editModal();
+            deleteModal();
+
+        });
 });
